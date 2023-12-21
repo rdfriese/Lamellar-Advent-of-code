1b485be8eb7d91b3b5caf71672a38bb1a485f942
rdfriese <rdfriese@WCDesktop>
2023-12-21 10:02:47 -0800

    day 18 serial

--- 2023/src/day18.rs
+++ 2023/src/day18.rs
@@ -5,7 +5,10 @@ use std::sync::{Arc, RwLock};
 use crate::WORLD;
 use aoc_runner_derive::{aoc, aoc_generator};
 use lamellar::active_messaging::prelude::*;
-//maybe we need to return a vec of valid splits
+
+// This is a pretty similar problem to day 10,
+// but bewteen then and now, I stumbled upon the Shoelace formula
+// for computing the area of a polygon
 
 // need to construct a LamellarWorld
 // but only one can be constructed per execution
@@ -15,10 +18,8 @@ use lamellar::active_messaging::prelude::*;
 pub fn part_1(_input: &str) -> u32 {
     WORLD.num_pes() as u32
 }
-#[aoc_generator(day18)]
-fn parse(input: &str) -> (Arc<Vec<(Dir, usize, Trench)>>, usize, usize) {
-    let mut upper_h = 0;
-    let mut upper_w = 0;
+#[aoc_generator(day18, part1)]
+fn parse(input: &str) -> Arc<Vec<(Dir, isize)>> {
     let data = input
         .lines()
         .map(|line| {
@@ -27,23 +28,15 @@ fn parse(input: &str) -> (Arc<Vec<(Dir, usize, Trench)>>, usize, usize) {
             let num = vals.next().unwrap().parse::<usize>().unwrap();
             let dir = match dir {
                 "U" => Dir::Up,
-                "D" => {
-                    upper_h += num as usize;
-                    Dir::Down
-                }
+                "D" => Dir::Down,
                 "L" => Dir::Left,
-                "R" => {
-                    upper_w += num as usize;
-                    Dir::Right
-                }
+                "R" => Dir::Right,
                 _ => unreachable!(),
             };
-
-            // let color = u32::from_str_radix(vals.next().unwrap(), 16).unwrap();
-            (dir, num, Trench::Edge(0))
+            (dir, num as isize)
         })
         .collect::<Vec<_>>();
-    (Arc::new(data), upper_h, upper_w)
+    Arc::new(data)
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -54,203 +47,52 @@ pub enum Dir {
     Right,
 }
 
-pub enum Trench {
-    Edge(u32),
-    Interior,
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum Entry {
-    Edge(isize),
-    Line(isize, isize),
-}
-
-impl Ord for Entry {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // println!("{:?} {:?}", self, other);
-        match (self, other) {
-            (Entry::Edge(a), Entry::Edge(b)) => a.cmp(b),
-            (Entry::Edge(a), Entry::Line(b, c)) => a.cmp(b).then_with(|| a.cmp(c)),
-            (Entry::Line(a, b), Entry::Edge(c)) => a.cmp(c).then_with(|| b.cmp(c)),
-            (Entry::Line(a, b), Entry::Line(c, d)) => a.cmp(c).then_with(|| b.cmp(d)),
-        }
+fn calc_area(data: &[(Dir, isize)]) -> isize {
+    let mut area = 0;
+    let mut perimiter = 0;
+    let mut previous = (0isize, 0isize);
+    let mut current = previous;
+    for (dir, num) in data.iter() {
+        previous = current;
+        current = match dir {
+            Dir::Up => (current.0, current.1 - num),
+            Dir::Down => (current.0, current.1 + num),
+            Dir::Left => (current.0 - num, current.1),
+            Dir::Right => (current.0 + num, current.1),
+        };
+        area += (previous.0 * current.1) - (previous.1 * current.0);
+        perimiter += num;
     }
-}
-
-impl PartialOrd for Entry {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-fn add_entry(lines: &mut Vec<Entry>, new: Entry) {
-    // println!("{:?}", new);
-    let valid = true;
-    for i in 0..lines.len() {
-        match (new, lines[i]) {
-            (Entry::Edge(a), Entry::Edge(b)) => {
-                if a == b {
-                    return; //already exists
-                }
-            }
-            (Entry::Edge(a), Entry::Line(b, c)) => {
-                if a == b || a == c {
-                    return;
-                }
-            }
-            (Entry::Line(a, b), Entry::Edge(c)) => {
-                if a == c || b == c {
-                    // edge exists on line
-                    lines[i] = Entry::Line(a, b);
-                    return;
-                }
-            }
-            (Entry::Line(a, b), Entry::Line(c, d)) => {
-                if a <= c && b >= d {
-                    //new line overlaps old line
-                    lines[i] = Entry::Line(a, b);
-                    return;
-                } else if a > c && b < d {
-                    //old line overlaps new line
-                    return;
-                } else if a <= c && b >= c && b <= d {
-                    //partial overlap
-                    lines[i] = Entry::Line(a, d);
-                    return;
-                } else if a >= c && a <= d && b >= d {
-                    lines[i] = Entry::Line(c, b);
-                    return;
-                }
-            }
-        }
-    }
-    lines.push(new);
+    //based on picks theorem
+    area / 2 + perimiter / 2 + 1
 }
 
 #[aoc(day18, part1, serial)]
-pub fn part_1_serial(
-    (data, upper_h, upper_w): &(Arc<Vec<(Dir, usize, Trench)>>, usize, usize),
-) -> isize {
-    let mut sum = 0;
-    let mut lines = BTreeMap::new();
-    let mut lines2 = BTreeMap::new();
-    let mut cur_i = 0isize;
-    let mut cur_j = 0;
-    // let mut min_i = 0;
-    // let mut min_j = 0;
-    for (dir, num, __) in data.iter() {
-        // println!("{lines:?}");
-        // println!("{dir:?}");
-        let num = *num as isize;
-        match dir {
-            Dir::Up => {
-                for i in cur_i as isize - num..cur_i as isize {
-                    add_entry(
-                        &mut lines.entry(i).or_insert(Vec::new()),
-                        Entry::Edge(cur_j),
-                    );
-                    lines2
-                        .entry(i)
-                        .or_insert(Vec::new())
-                        .push(Entry::Edge(cur_j));
-                }
-                cur_i -= num as isize;
-            }
-            Dir::Down => {
-                for i in cur_i as isize..cur_i as isize + num {
-                    add_entry(
-                        &mut lines.entry(i).or_insert(Vec::new()),
-                        Entry::Edge(cur_j),
-                    );
-                    lines2
-                        .entry(i)
-                        .or_insert(Vec::new())
-                        .push(Entry::Edge(cur_j));
-                }
-                cur_i += num as isize;
-            }
-            Dir::Left => {
-                add_entry(
-                    &mut lines.entry(cur_i as isize).or_insert(Vec::new()),
-                    Entry::Line(cur_j - num as isize, cur_j),
-                );
-                lines2
-                    .entry(cur_i as isize)
-                    .or_insert(Vec::new())
-                    .push(Entry::Line(cur_j - num as isize, cur_j));
-                cur_j -= num as isize;
-            }
-            Dir::Right => {
-                add_entry(
-                    &mut lines.entry(cur_i as isize).or_insert(Vec::new()),
-                    Entry::Line(cur_j, cur_j + num as isize),
-                );
-                lines2
-                    .entry(cur_i as isize)
-                    .or_insert(Vec::new())
-                    .push(Entry::Line(cur_j, cur_j + num as isize));
-                cur_j += num as isize;
-            }
-        }
-        // println!("{lines:?}");
-        // println!("");
-    }
-    for (i, line) in lines.iter_mut() {
-        line.sort();
-        println!("{i} {:?}", line);
-        let mut line2 = lines2.get(i).unwrap().clone();
-        line2.sort();
-        println!("{i} {:?}", line2);
-        // for window in lines.windows(2) {
-        let mut i = 0;
-        let mut tempsum = 0;
-        while i < line.len() {
-            if i % 2 == 0 {
-                println!("{:?}", line[i]);
-                match line[i] {
-                    Entry::Line(a, b) => {
-                        tempsum += (b - a) + 1;
-                        i += 1;
-                        if i < line.len() {
-                            match line[i] {
-                                Entry::Line(c, d) => {
-                                    tempsum += d - b; // already accounted for inclusive range
-                                }
-                                Entry::Edge(c) => {
-                                    tempsum += c - b; //already accounted for inclusive range
-                                }
-                            }
-                        }
-                    }
-                    Entry::Edge(a) => {
-                        i += 1; //assume that if we are on an even index as an edge there must be at least one other edge
-                        match line[i] {
-                            Entry::Line(b, c) => {
-                                tempsum += c - b;
-                                tempsum += (b - a) + 1;
-                            }
-                            Entry::Edge(b) => {
-                                tempsum += (b - a) + 1;
-                            }
-                        }
-                    }
-                }
-                i += 1;
-            } else {
-                //only way we get here is if it is a line
-                match line[i] {
-                    Entry::Line(a, b) => {
-                        tempsum += (b - a) + 1;
-                    }
-                    Entry::Edge(a) => {
-                        println!(" can we be here? {:?}", line[i]);
-                    }
-                }
-                i += 1;
-            }
-        }
-        sum += tempsum;
-        println!("{} {}", tempsum, sum);
-    }
-    sum
+pub fn part_1_serial(data: &Arc<Vec<(Dir, isize)>>) -> isize {
+    calc_area(&data)
+}
+
+#[aoc_generator(day18, part2)]
+fn parse2(input: &str) -> Arc<Vec<(Dir, isize)>> {
+    let data = input
+        .lines()
+        .map(|line| {
+            let vals = line.split('#').last().unwrap();
+            let num = usize::from_str_radix(&vals[0..5], 16).unwrap();
+            let dir = match vals.chars().nth(5).unwrap() {
+                '0' => Dir::Right,
+                '1' => Dir::Down,
+                '2' => Dir::Left,
+                '3' => Dir::Up,
+                _ => unreachable!(),
+            };
+            (dir, num as isize)
+        })
+        .collect::<Vec<_>>();
+    Arc::new(data)
+}
+
+#[aoc(day18, part2, serial)]
+pub fn part_2_serial(data: &Arc<Vec<(Dir, isize)>>) -> isize {
+    calc_area(&data)
 }
